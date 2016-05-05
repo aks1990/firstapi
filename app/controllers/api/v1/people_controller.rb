abb9e20c814b6fed11688c1e22e0a3c570ffe877
anshulkthr <anshulkthr34@gmail.com>
2016-05-05 13:05:00 +0530

    create action

--- app/controllers/api/v1/people_controller.rb
+++ app/controllers/api/v1/people_controller.rb
@@ -5,4 +5,23 @@ class API::V1::PeopleController < ApplicationController
       format.json { render :json => @people }
     end
 	end
+
+	def create
+		@people = Person.new(person_params)
+
+    respond_to do |format|
+      if @people.save
+        format.json { render :json => @people }
+      else
+        format.json { render :json => @people.errors, status: :unprocessable_entity }
+      end
+    end
+	end
+
+	private
+  
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def person_params
+      params.require(:person).permit(:name, :city, :country)
+    end
 end
